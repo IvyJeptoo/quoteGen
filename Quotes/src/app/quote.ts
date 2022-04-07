b50b83e5181d2d17de0d7653e462925782b2970f
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-07 19:59:34 +0300

    add delete functionality

--- Quotes/src/app/quote.ts
+++ Quotes/src/app/quote.ts
@@ -1,7 +1,12 @@
 export class Quote {
-    id!: number;
-    quote!: string;
-    author!: string;
+    // id!: number;
+    // quote!: string;
+    // description!: string;
+    // author!: string;
     // submitter!: string;
+    showDescription: boolean;
+    constructor(public id:number, public quote: string, public description: string, public author: string){
+        this.showDescription=false;
+    }
 
 }
